7989f092e0d876b89ca7f4f6acddd07af1540b5c
Caio <kcaiosouza@gmail.com>
2024-11-07 15:09:04 -0300

    :sparkles: feat: Rota de buscar Músicas por titulo, letra e idioma

--- src/controllers/songs.ts
+++ src/controllers/songs.ts
@@ -2,22 +2,32 @@ import { FastifyInstance } from 'fastify';
 import prisma from '../plugins/prisma';
 import { z } from 'zod';
 
+const songSchema = z.object({
+  title: z.string().min(1),
+  slug: z.string().min(1),
+  album_id: z.string(),
+  file_url: z.string().url(),
+  artist_id: z.string(),
+  language: z.enum(['pt_BR', 'en_US', 'es_ES', 'de_DE', 'fr_FR', 'it_IT', 'af_AF']), // Ajustar em caso de alteração no backend
+  lyrics: z.string().optional(),
+  chords: z.string().optional(),
+  duration: z.number().positive(),
+});
+
+const languegeSchema = z.object({
+  enum: z.enum(['pt_BR', 'en_US', 'es_ES', 'de_DE', 'fr_FR', 'it_IT', 'af_AF']), // Ajustar em caso de alteração no backend
+});
+
+const searchSongSchema = z.object({
+  language: z.enum(['pt_BR', 'en_US', 'es_ES', 'de_DE', 'fr_FR', 'it_IT', 'af_AF']),
+  query: z.string().min(1),
+})
+
 export async function songRoutes(fastify: FastifyInstance) {
   // Criar uma nova música
   fastify.post('/', async (request, reply) => {
-    const songSchema = z.object({
-      title: z.string().min(1),
-      slug: z.string().min(1),
-      album_id: z.string(),
-      file_url: z.string().url(),
-      artist_id: z.string(),
-      language: z.enum(['pt_BR', 'en_US', 'es_ES', 'de_DE', 'fr_FR', 'it_IT', 'af_AF']), // Ajustar em caso de alteração no backend
-      lyrics: z.string().optional(),
-      chords: z.string().optional(),
-      duration: z.number().positive(),
-    });
-
     const parsedData = songSchema.safeParse(request.body);
+    
     if (!parsedData.success) {
       return reply.status(400).send(parsedData.error.format());
     }
@@ -36,6 +46,7 @@ export async function songRoutes(fastify: FastifyInstance) {
           chords: parsedData.data.chords ? parsedData.data.chords : null,
         }
       });
+
       return reply.status(201).send(newSong);
     } catch (error) {
       return reply.status(500).send({ error: 'Erro ao criar a música.' });
@@ -62,10 +73,6 @@ export async function songRoutes(fastify: FastifyInstance) {
   
   // Todas as músicas por Idioma
   fastify.get<{ Params: { enum: string } }>('/all/language/:enum', async (request, reply) => {
-    const languegeSchema = z.object({
-      enum: z.enum(['pt_BR', 'en_US', 'es_ES', 'de_DE', 'fr_FR', 'it_IT', 'af_AF']), // Ajustar em caso de alteração no backend
-    });
-
     const data = languegeSchema.parse(request.params);
 
     try {
@@ -87,4 +94,34 @@ export async function songRoutes(fastify: FastifyInstance) {
     }
   })
 
+  fastify.get('/search', async (request, reply) => {
+    const { data } = searchSongSchema.safeParse(request.query);
+
+    try {
+      const musics = await prisma.song.findMany({
+        where: {
+          OR: [
+            { title: { startsWith: data?.query?.toLowerCase(), } },
+            { lyrics: { contains: data?.query?.toLowerCase(), } },
+          ],
+          AND: [
+            { language: data?.language ? data?.language : undefined },
+          ],
+        },
+      });
+      if(musics.length > 0) {
+        return reply.send(musics);
+      }else {
+        return reply.status(404).send({
+          error: 404,
+          message: 'Nenhuma música foi encontrada',
+        })
+      }
+    }catch (error) {
+      return reply.status(500).send({
+        error: 500,
+        message: 'Erro no banco de dados',
+      })
+    }
+  })
 }
