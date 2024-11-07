9513e632873e082a5c9b1bd70f7c0c64043c0f1a
Caio <kcaiosouza@gmail.com>
2024-11-07 17:14:09 -0300

    :sparkles: feat: pesquisar música por album e artista

--- src/controllers/songs.ts
+++ src/controllers/songs.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import prisma from '../plugins/prisma';
-import { z } from 'zod';
+import { string, z } from 'zod';
 
 const songSchema = z.object({
   title: z.string().min(1),
@@ -19,36 +19,40 @@ const languegeSchema = z.object({
 });
 
 const searchSongSchema = z.object({
-  language: z.enum(['pt_BR', 'en_US', 'es_ES', 'de_DE', 'fr_FR', 'it_IT', 'af_AF']),
+  language: z.enum(['pt_BR', 'en_US', 'es_ES', 'de_DE', 'fr_FR', 'it_IT', 'af_AF']).optional(),
   query: z.string().min(1),
+})
+
+const getSongsByAlbumIdSchema = z.object({
+  album_id: z.string().uuid()
+})
+
+const getSongsByArtistIdSchema = z.object({
+  artist_id: z.string().uuid()
 })
 
 export async function songRoutes(fastify: FastifyInstance) {
   // Criar uma nova música
   fastify.post('/', async (request, reply) => {
-    const parsedData = songSchema.safeParse(request.body);
-    
-    if (!parsedData.success) {
-      return reply.status(400).send(parsedData.error.format());
-    }
+    const data = songSchema.parse(request.body);
 
     try {
       const newSong = await prisma.song.create({
         data: {
-          title: parsedData.data.title,
-          duration: parsedData.data.duration,
-          language: parsedData.data.language,
-          slug: parsedData.data.slug,
-          album_id: parsedData.data.album_id,
-          artist_id: parsedData.data.artist_id,
-          file_url: parsedData.data.file_url,
-          lyrics: parsedData.data.lyrics ? parsedData.data.lyrics : null,
-          chords: parsedData.data.chords ? parsedData.data.chords : null,
+          title: data.title,
+          duration: data.duration,
+          language: data.language,
+          slug: data.slug,
+          album_id: data.album_id,
+          artist_id: data.artist_id,
+          file_url: data.file_url,
+          lyrics: data.lyrics ? data.lyrics : null,
+          chords: data.chords ? data.chords : null,
         }
       });
 
       return reply.status(201).send(newSong);
-    } catch (error) {
+    } catch(err) {
       return reply.status(500).send({ error: 'Erro ao criar a música.' });
     }
   });
@@ -105,9 +109,13 @@ export async function songRoutes(fastify: FastifyInstance) {
             { lyrics: { contains: data?.query?.toLowerCase(), } },
           ],
           AND: [
-            { language: data?.language ? data?.language : undefined },
+            data?.language ? { language: data?.language } : {},
           ],
         },
+        include: {
+          album: true,
+          artist: true,
+        }
       });
       if(musics.length > 0) {
         return reply.send(musics);
@@ -117,11 +125,76 @@ export async function songRoutes(fastify: FastifyInstance) {
           message: 'Nenhuma música foi encontrada',
         })
       }
-    }catch (error) {
+    }catch (err) {
       return reply.status(500).send({
         error: 500,
         message: 'Erro no banco de dados',
       })
     }
   })
+
+  fastify.get('/album/:album_id', async (request, reply) => {
+    const data = getSongsByAlbumIdSchema.parse(request.params);
+    
+    try {
+      const musics = await prisma.song.findMany({
+        where: {
+          album_id: data.album_id,
+        },
+        include: {
+          album: true,
+          artist: true,
+        }
+      });
+
+      if(musics.length > 0) {
+        return reply.send(musics);
+      }else {
+        return reply.status(404).send({
+          error: 404,
+          message: 'Nenhuma música foi encontrada',
+        })
+      }
+    }catch(err) {
+      return reply.status(500).send({
+        error: 500,
+        message: 'Erro no banco de dados',
+      })
+    }
+  })
+
+  fastify.get('/artist/:artist_id', async (request, reply) => {
+    const data = getSongsByArtistIdSchema.parse(request.params);
+  
+    try {
+      const musics = await prisma.song.findMany({
+        where: {
+          artist_id: data.artist_id,
+        },
+        include: {
+          album: true,
+          artist: true,
+        },
+        orderBy: {
+          album: {
+            title: 'asc',
+          },
+        },
+      });
+  
+      if (musics.length > 0) {
+        return reply.send(musics);
+      } else {
+        return reply.status(404).send({
+          error: 404,
+          message: 'Nenhuma música foi encontrada',
+        });
+      }
+    }catch(err) {
+      return reply.status(500).send({
+        error: 500,
+        message: 'Erro no banco de dados',
+      });
+    }
+  });
 }
